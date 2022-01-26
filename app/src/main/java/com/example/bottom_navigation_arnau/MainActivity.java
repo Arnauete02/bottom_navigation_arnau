9d7e899cb42655211055472e7228390826ad9cf9
Arnauete02 <agraciataberner02@gmail.com>
2022-01-26 08:46:11 +0100

    first commit

---
+++ app/src/main/java/com/example/bottom_navigation_arnau/MainActivity.java
@@ -0,0 +1,67 @@
+package com.example.bottom_navigation_arnau;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView mMainNav;
+    private FrameLayout mMainFrame;
+
+    private HomeFragment homeFragment;
+    private ProfileFragment profileFragment;
+    private SettingsFragment settingsFragment;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mMainNav = findViewById(R.id.main_nav);
+        mMainFrame = findViewById(R.id.main_frame);
+        homeFragment = new HomeFragment();
+        profileFragment = new ProfileFragment();
+        settingsFragment = new SettingsFragment();
+
+        setFragment(homeFragment);
+
+        mMainNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.nav_home:
+                        setFragment(homeFragment);
+                        return true;
+                    case R.id.nav_profile:
+                        setFragment(profileFragment);
+                        return true;
+                    case R.id.nav_settings:
+                        setFragment(settingsFragment);
+                        return true;
+                    default:
+                        setFragment(homeFragment);
+                        return false;
+                }
+
+            }
+        });
+
+        homeFragment = HomeFragment.newInstance("Home1", "Home2");
+    }
+
+    private void setFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.main_frame, fragment);
+        fragmentTransaction.commit();
+    }
+}
